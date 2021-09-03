f7a719270ad425bf02cb8de8bf6397f2f682beae
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2021-09-03 17:28:13 +0700

    getdata in last 7days

--- src/Pages/CountriesSummary.js
+++ src/Pages/CountriesSummary.js
@@ -2,36 +2,24 @@ import axios from "axios"
 import { useState, useEffect } from "react"
 import React from "react"
 import { DataGrid } from '@material-ui/data-grid';
-
+import { Doughnut } from 'react-chartjs-2';
 export default function CountriesSummary() {
   const [countriesSummary, setCountriesSummary] = useState([]);
   useEffect(() => {
     axios
-      .get('https://5ymeqj1dbk.execute-api.us-east-1.amazonaws.com/covid2/summaries')
+      .get('https://mddz75k2w0.execute-api.us-east-1.amazonaws.com/prod/summaries')
       .then((res) => {
         setCountriesSummary(res.data.CovidSummarys);
-    
+
       })
       .catch((err) => console.error(err))
   }, []
   )
 
-//   export const fetchData = (tableName) => {
-//     var params = {
-//         TableName: tableName
-//     }
-
-//     docClient.scan(params, function (err, data) {
-//         if (!err) {
-//             console.log(data)
-//         }
-//     })
-// }
-
   const columns = [
     { field: 'indexNumber', headerName: 'index', width: 300 },
     { field: 'id', headerName: 'id', width: 300 },
-    
+
     {
       field: 'Country',
       headerName: 'Country',
@@ -71,26 +59,121 @@ export default function CountriesSummary() {
     },
   ];
 
-  
-  const rows = [];
+  var vietNamCollection = [];
+  var singaporeCollection = [];
+  var cambodiaCollection = [];
+  var malaysiaCollection = [];
+  var thailandCollection = [];
+  var rows = [];
   var count = 0;
   for (const country of countriesSummary) {
-    
+
     country['indexNumber'] = count;
     count++;
     country['id'] = country['indexNumber'];
     rows.push(country);
   }
-  console.log(rows);
+
+
+  var currentDate = new Date();
+  var sevenDayAgo = new Date();
+  currentDate.setDate(currentDate.getDate());
+  sevenDayAgo.setDate(sevenDayAgo.getDate() - 7);
+
+  for (const country of countriesSummary) {
+    var dayInArray = new Date(country['Date']);
+    dayInArray.setDate(dayInArray.getDate());
+
+    if (dayInArray.getTime() >= sevenDayAgo.getTime() && dayInArray.getTime() <= currentDate.getTime()) {
+      if (country['Country'] === 'Viet Nam') {
+        vietNamCollection.push(country);
+      }
+      if (country['Country'] === 'Singapore') {
+        singaporeCollection.push(country);
+      }
+      if (country['Country'] === 'Cambodia') {
+        cambodiaCollection.push(country);
+      }
+      if (country['Country'] === 'Malaysia') {
+        malaysiaCollection.push(country);
+      }
+      if (country['Country'] === 'ThaiLand') {
+        thailandCollection.push(country);
+      }
+    }
+  }
+ 
+  for (const vn of vietNamCollection) {
+    console.log(new Date(vn.Date).toLocaleDateString());
+  }
+ 
+
+
+  // console.log(rows);
   return (
-    <div style={{ height: 500, width: '100%' }}>
+    < div style={{ height: 500, width: '100%' }}>
       <DataGrid
-        rows={rows.map( (r) => {return r})}
+        rows={rows.map((r) => { return r })}
         columns={columns}
         rowsPerPageOptions={[5, 10, 20, 50, 100]}
         checkboxSelection
         disableSelectionOnClick
       />
+      <div>
+      <Doughnut
+        data={{
+          labels: ['VietNam', 'ThaiLand', 'Cambodia', 'Malaysia', 'Singapore'],
+          datasets: [
+            {
+              label: '# of votes',
+              data: [12, 19, 3, 5, 2],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 4, 
+            },
+            // {
+            //   label: 'Quantity',
+            //   data: [47, 52, 67, 58, 9, 50],
+            //   backgroundColor: 'orange',
+            //   borderColor: 'red',
+            // },
+          ],
+        }}
+        height={400}
+        width={600}
+        options={{
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+          legend: {
+            labels: {
+              fontSize: 25,
+            },
+          },
+        }}
+      />
+    </div>
     </div>
   )
 }
