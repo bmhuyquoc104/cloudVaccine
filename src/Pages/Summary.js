833f443a292e18e0e11bcef660dcdf438507f305
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2021-09-04 22:15:22 +0700

    Update Summary.js

--- src/Pages/Summary.js
+++ src/Pages/Summary.js
@@ -51,7 +51,7 @@ export default function Summary() {
         <div className="summary">
             <h1 style={{ color: "rgb(86, 100, 210)" }}>huy</h1>
             {/* <a href="https://imgur.com/3kkytsv"><img src="https://i.imgur.com/3kkytsv.png" title="source: imgur.com" /></a> */}
-            <img src="https://i.imgur.com/3kkytsv.png"></img>
+            <img src="https://i.imgur.com/ToDMYPT.png"></img>
         </div>
     )
 }
