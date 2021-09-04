35fd0c7408a24d298b4e85629989f709d471355e
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2021-09-04 21:27:02 +0700

    modify button in vaccine

--- src/App.js
+++ src/App.js
@@ -32,57 +32,6 @@ function App() {
   return (
     <Container className={classes.root}>
       <Sidenav />
-      {/* <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/registration">Registration</Link>
-            </li>
-            <li>
-              <Link to="/review">Review</Link>
-            </li>
-            <li>
-              <Link to="/vaccine">Vaccine</Link>
-            </li>
-            <li>
-              <Link to="/countriesSummary">CountriesSummary</Link>
-            </li>
-            <li>
-              <Link to="/signUp">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/signIn">Sign In</Link>
-            </li>
-            <li>
-              <Link to="/Summary">Summary</Link>
-            </li>
-          </ul>
-
-          <Switch>
-            <Route path="/registration">
-              <Registration />
-            </Route>
-            <Route path="/review">
-              <Review />
-            </Route>
-            <Route path="/vaccine">
-              <Vaccine />
-            </Route>
-            <Route path="/countriesSummary">
-              <CountriesSummary />
-            </Route>
-            <Route path="/signIn">
-              <Login />
-            </Route>
-            <Route path="/signUp">
-              <Signup />
-            </Route>
-            <Route path="/Summary">
-              <Summary />
-            </Route>
-          </Switch>
-        </div>
-      </Router> */}
     </Container>
   );
 }
--- src/Pages/Vaccine.js
+++ src/Pages/Vaccine.js
@@ -32,9 +32,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const configuration = {
-    region: 'us-east-1',
-    secretAccessKey: 'RijJPrAkst+a132dzazw+u9ssMWZsbttvvcVOE32',
-    accessKeyId: 'AKIA4Y5CM62A3AGGGW2W'
+  region: 'us-east-1',
+  secretAccessKey: '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',
+  accessKeyId: 'AKIA4Y5CM62A3AGGGW2W'
 }
 
 
@@ -42,84 +42,93 @@ AWS.config.update(configuration)
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const putData = (tableName, data) => {
-    var params = {
-        TableName: tableName,
-        Item: data
-    }
+  var params = {
+    TableName: tableName,
+    Item: data
+  }
 
-    docClient.put(params, function (err, data) {
-        if (err) {
-            console.log('Error', err)
-        } else {
-            console.log('Success', data)
-        }
-    })
+  docClient.put(params, function (err, data) {
+    if (err) {
+      console.log('Error', err)
+    } else {
+      console.log('Success', data)
+    }
+  })
 }
 
 
 
 export default function Vaccine() {
-    const [vaccines, setVaccines] = useState([]);
-    useEffect(() => {
-        axios
-            .get('https://75hpp2g9n5.execute-api.us-east-1.amazonaws.com/vac/vaccines')
-            .then((res) => {
-                setVaccines(res.data.Vaccines);
-            })
-            .catch((err) => console.error(err))
-    }, []
-    )
-    console.log(vaccines)
+  const [vaccines, setVaccines] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://75hpp2g9n5.execute-api.us-east-1.amazonaws.com/vac/vaccines')
+      .then((res) => {
+        setVaccines(res.data.Vaccines);
+      })
+      .catch((err) => console.error(err))
+  }, []
+  )
+  console.log(vaccines)
 
-   var click = false;
-    const addLike = async (idx) =>{
-        if(click === false ){
-            vaccines[idx].like +=1;
-            await putData('vaccine-covid',vaccines[idx]);
-            console.log(vaccines[idx]);
-            click = true;
-        }
-        else{
-            vaccines[idx].like -=1;
-            await putData('vaccine-covid',vaccines[idx]);
-            console.log(vaccines[idx]);
-            click = false;
-        }
-    }
+  var click = false;
+  var allHeartContainers = document.getElementsByClassName("MuiGrid-root MuiGrid-item");
+  var ThuCoBeXinhXan;
+
+  for (const item of allHeartContainers) {
+    ThuCoBeXinhXan = item.getElementsByTagName('div');
+  }
+
+  console.log(ThuCoBeXinhXan)
   
-    const [spacing, setSpacing] = React.useState(2);
-    const classes = useStyles();
+  // <button class="MuiButtonBase-root MuiIconButton-root" tabindex="0" type="button"><span class="MuiIconButton-label"><svg class="MuiSvgIcon-root makeStyles-text-23" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path></svg></span><span class="MuiTouchRipple-root"></span></button>
+  const addLike = async (idx) => {
+    if (click === false) {
+      vaccines[idx].like += 1;
+      await putData('vaccine-covid', vaccines[idx]);
+      console.log(vaccines[idx]);
+      click = true;
+    }
+    else {
+      vaccines[idx].like -= 1;
+      await putData('vaccine-covid', vaccines[idx]);
+      console.log(vaccines[idx]);
+      click = false;
+    }
+  }
 
-    return (
-            <Grid container spacing={2} style={{paddingTop: "20px", paddingLeft: "50px", paddingRight: "50px"}} className={classes.root}>
-              <Grid item xs={12}>
-                <Grid container justifyContent="center" spacing={spacing}>
-                  {vaccines.map((vaccine, idx) => {
-                    return (
-                      <Grid key={`vaccine${idx}`} item>
-                        <Card className={classes.paper}>
-                          <CardActionArea>
-                              <CardContent>
-                                  <CardMedia image={'https://i.imgur.com/3kkytsv.png'} style={{ width: "250px", height: "300px", margin: "auto" }} alt="..."/>
-                              </CardContent>
-                          </CardActionArea>
-                          
-                          <CardContent className={classes.bot}>
-                            <Typography className={classes.text}><b>{vaccine.name}</b></Typography>
-                            <Typography className={classes.text}><b>ID:</b> {vaccine.id}</Typography>
-                            <Typography className={classes.text}><b>Efficiency:</b> {vaccine.effecientcy}</Typography>
-                            <Typography className={classes.text}><b>Like:</b> {vaccine.like}</Typography>
-                            <Typography className={classes.text}><b>Country:</b> {vaccine.country}</Typography>
-                            <IconButton onClick={() => addLike(idx)}>  
-                              <FavoriteIcon className={classes.text}/>
-                            </IconButton>
-                          </CardContent>
-                        </Card>
-                      </Grid>
-                    )
-                  })}
-                </Grid>
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
+
+  return (
+    <Grid container spacing={2} style={{ paddingTop: "20px", paddingLeft: "50px", paddingRight: "50px" }} className={classes.root}>
+      <Grid item xs={12}>
+        <Grid container justifyContent="center" spacing={spacing}>
+          {vaccines.map((vaccine, idx) => {
+            return (
+              <Grid key={`vaccine${idx}`} item>
+                <Card className={classes.paper}>
+                  <CardActionArea>
+                    <CardContent>
+                      <CardMedia image={'https://i.imgur.com/3kkytsv.png'} style={{ width: "250px", height: "300px", margin: "auto" }} alt="..." />
+                    </CardContent>
+                  </CardActionArea>
+
+                  <CardContent className={classes.bot}>
+                    <Typography className={classes.text}><b>{vaccine.name}</b></Typography>
+                    <Typography className={classes.text}><b>ID:</b> {vaccine.id}</Typography>
+                    <Typography className={classes.text}><b>Efficiency:</b> {vaccine.effecientcy}</Typography>
+                    <Typography className={classes.text}><IconButton onClick={() => addLike(idx)}>
+                    <FavoriteIcon className={classes.text} />
+                    </IconButton> {vaccine.like}</Typography>
+                    <Typography className={classes.text}><b>Country:</b> {vaccine.country}</Typography>
+                  </CardContent>
+                </Card>
               </Grid>
-            </Grid>
-    );
+            )
+          })}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
