885d0a6734a3885e99f537cd07e71cf21f9e6f69
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2021-09-05 13:01:48 +0700

    remove button support US (conflict to like fuction)

--- src/Pages/Review.js
+++ src/Pages/Review.js
@@ -82,14 +82,14 @@ export default function Review() {
     }, []
     )
 
-    
+
 
 
     var click = false;
     var allButtons = document.getElementsByClassName("MuiButton-label");
     const addLikeOrDislike = async (idx, mode) => {
-        var likeButton = allButtons[idx * 3];
-        var dislikeButton = allButtons[idx * 3 + 1];
+        var likeButton = allButtons[idx * 2];
+        var dislikeButton = allButtons[idx * 2 + 1];
         try {
             if (click === false) {
                 if (mode === 1) {
@@ -124,7 +124,7 @@ export default function Review() {
 
     const [spacing, setSpacing] = React.useState(2);
     const classes = useStyles();
-
+    
     return (
         <div>
             <Grid container spacing={2} style={{ paddingTop: "20px", paddingLeft: "50px", paddingRight: "50px" }} className={classes.root}>
@@ -164,13 +164,7 @@ export default function Review() {
                                             >
                                                 {review.dislike}
                                             </Button>
-                                            <Button
-                                                size="medium"
-                                                startIcon={<ReplyIcon />}
-                                                className={classes.icon}
-                                            >
-                                                Reply
-                                            </Button>
+                                         
                                         </CardActions>
                                     </Card>
                                 </Grid>
