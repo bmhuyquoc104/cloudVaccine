4cfe3a763409975ed2ce33fe0252979a5926ae5a
Huynh Chau <48887152+ChauHuynh1@users.noreply.github.com>
2021-09-07 17:03:05 +0700

    Delete src/lib directory

--- src/lib/sesClient.js
+++
@@ -1,7 +0,0 @@
-const { SESClient } = require("@aws-sdk/client-ses");
-
-// Set the AWS Region.
-const REGION = "us-east-1"; //e.g. "us-east-1"
-// Create SES service object.
-const sesClient = new SESClient({ region: REGION });
-module.exports =  { sesClient };
