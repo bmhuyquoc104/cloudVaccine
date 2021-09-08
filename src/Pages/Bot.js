d2f56e96188c3742150a27eb4d1ff0e10b04a563
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2021-09-08 10:26:08 +0700

    made simple bot page

--- src/Pages/Bot.js
+++ src/Pages/Bot.js
@@ -1,13 +1,41 @@
 import React from 'react';
+// Buttons
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
-export default function Bot(){
-    return(
+// Modals
+import Form from 'react-bootstrap/Form';
+
+export default function Bot() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const initialState = {
+        question :''
+    }
+
+    return (
         <div className="bot">
-            <h1>This page is made for bot</h1>
-            <form className="bot">
-                <input type as textarea className="" placeholder="Say to our bot">
-                </input>
-            </form>
+            <h1 style={{color: 'white'}}>This page is made for bot</h1>
+            <Form id="myform"  onSubmit={handleSubmit} >
+                    <Form.Group className="mb-3" controlId="formBasicDate">
+                        <Form.Label>Ask me please</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            placeholder="Ask Me Please !!!!"
+                            required
+                            onChange={(e) => initialState['question'] = e.target.value}
+                        />
+
+                    </Form.Group>
+                    <ButtonGroup className="mb-2">
+                        <Button type="submit" style={{ fontWeight: 'bold', backgroundImage: 'linear-gradient(45deg, #20BF55 30%, #01BAEF 90%)', border: 0 }}>
+                            Search
+                        </Button>
+                    </ButtonGroup>
+            </Form>
         </div>
     )
 }
