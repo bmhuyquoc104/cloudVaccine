38859dc48e42f50e1a9bbbb7b848ea70a83f2992
[bmhuyquoc104] <[s3823236@rmit.edu.vn]>
2021-09-08 17:16:21 +0700

    change container tag to div tag in App.js to make page responsive

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
-
 import Layout from './Components/Layout/Layout';
-import { makeStyles } from '@material-ui/core/styles';
-import { Container } from '@material-ui/core';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    background: 'linear-gradient(45deg, #380036 30%, #21CBF3 90%)',
-    width:'100vw',
-    height: '100%',
-    display: 'flex',
-    fontFamily: 'Roboto'
-  }
-}));
 
 function App() {
-  const classes = useStyles();
   return (
-    <Container className={classes.root}>
+    <div style={{background: 'linear-gradient(45deg, #380036 30%, #21CBF3 90%)', width:'100%'}}>
       <Layout />
-    </Container>
+    </div>
   );
 }
 
