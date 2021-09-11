75577662bf21f16696b55e05dd5681432f07a9d7
IuKim <s3805504@rmit.edu.vn>
2021-09-11 16:23:46 +0700

    Update

--- src/User/ChangeEmail.js
+++ src/User/ChangeEmail.js
@@ -2,6 +2,14 @@ import React, { useState, useContext } from "react";
 import { CognitoUserAttribute } from "amazon-cognito-identity-js";
 import { AccountContext } from "./Accounts";
 
+// Modals
+import Form from 'react-bootstrap/Form';
+
+import {
+  Box,
+  Button,
+} from '@material-ui/core';
+
 export default function ChangeEmail() {
   const [newEmail, setNewEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -27,23 +35,57 @@ export default function ChangeEmail() {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="email"
-          value={newEmail}
-          onChange={event => setNewEmail(event.target.value)}
-          placeholder="New Email"
-        />
+        <Form noValidate>
+            <Form.Group className="mb-3" controlId="formBasicEmail" style={{color: '#FE6B8B', fontWeight: 'bold' }}>
+              <Form.Label>New Email</Form.Label>
+              <Form.Control
+                name="email"
+                type="email"
+                placeholder="username@gmail.com"
+                required
+                onChange={event => setNewEmail(event.target.value)}
+                pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid email.
+              </Form.Control.Feedback>
+              <Form.Text className="text-muted">
+              </Form.Text>
+            </Form.Group>
 
-        <input
-          type="password"
-          value={password}
-          onChange={event => setPassword(event.target.value)}
-          placeholder="Change Password"
-        />
+            <Form.Group className="mb-3" controlId="formBasicFullName" style={{color: '#FE6B8B', fontWeight: 'bold' }}>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                name="password"
+                type="password"
+                placeholder="password"
+                required
+                onChange={event => setPassword(event.target.value)}
+                value={password}
+              />
+              <Form.Control.Feedback type="invalid">
+                Wrong password.
+              </Form.Control.Feedback>
+            </Form.Group>
+        </Form>
 
-        <button type="submit">Change email</button>
-      </form>
+        {/* // Old button
+        <button type='submit'> Sign in now</button> */}
+        <Box sx={{ py: 2 }} justifyContent="center">
+          <Button
+            //className={classes.bot}
+            color="secondary"
+            style={{letterSpacing:"4px",fontWeight: 'bold', backgroundImage: 'linear-gradient(45deg, #4c4177 30%, #473146  90%)', border: 0, width:"20vw"}}
+            // style={{ fontFamily: 'cursive',fontWeight: 'bold', backgroundImage: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', border: 0 }}
+            align="center"
+            type="submit"
+            size = "medium"
+            variant="contained"
+            onClick={onSubmit}
+          >
+            Change Email
+          </Button>
+        </Box>
     </div>
   );
 };
--- src/User/ChangePassword.js
+++ src/User/ChangePassword.js
@@ -1,6 +1,14 @@
 import React, { useState, useContext } from "react";
 import { AccountContext } from "./Accounts";
 
+// Modals
+import Form from 'react-bootstrap/Form';
+
+import {
+  Box,
+  Button,
+} from '@material-ui/core';
+
 export default function ChangePassword() {
   const [password, setPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
@@ -22,23 +30,55 @@ export default function ChangePassword() {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="password"
-          value={password}
-          onChange={event => setPassword(event.target.value)}
-          placeholder="Password"
-        />
+      <Form noValidate>
+            <Form.Group className="mb-3" controlId="formBasicFullName" style={{color: '#FE6B8B', fontWeight: 'bold' }}>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                name="password"
+                type="password"
+                placeholder="password"
+                required
+                onChange={event => setPassword(event.target.value)}
+                value={password}
+              />
+              <Form.Control.Feedback type="invalid">
+                Wrong password.
+              </Form.Control.Feedback>
+            </Form.Group>
 
-        <input
-          type="password"
-          value={newPassword}
-          onChange={event => setNewPassword(event.target.value)}
-          placeholder="New Password"
-        />
+            <Form.Group className="mb-3" controlId="formBasicFullName" style={{color: '#FE6B8B', fontWeight: 'bold' }}>
+              <Form.Label>New Password</Form.Label>
+              <Form.Control
+                name="newpassword"
+                type="password"
+                placeholder="password"
+                required
+                onChange={event => setNewPassword(event.target.value)}
+                value={newPassword}
+              />
+              <Form.Control.Feedback type="invalid">
+                New password doesn't match.
+              </Form.Control.Feedback>
+            </Form.Group>
+        </Form>
 
-        <button type="submit">Change password</button>
-      </form>
+        {/* // Old button
+        <button type='submit'> Sign in now</button> */}
+        <Box sx={{ py: 2 }} justifyContent="center">
+          <Button
+            //className={classes.bot}
+            color="secondary"
+            style={{letterSpacing:"4px",fontWeight: 'bold', backgroundImage: 'linear-gradient(45deg, #4c4177 30%, #473146  90%)', border: 0, width:"20vw"}}
+            // style={{ fontFamily: 'cursive',fontWeight: 'bold', backgroundImage: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', border: 0 }}
+            align="center"
+            type="submit"
+            size = "medium"
+            variant="contained"
+            onClick={onSubmit}
+          >
+            Change Password
+          </Button>
+        </Box>
     </div>
   );
 };
