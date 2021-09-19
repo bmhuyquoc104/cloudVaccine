f5febdc46933a471d79913617864dd5c6332d306
IuKim <s3805504@rmit.edu.vn>
2021-09-19 15:39:21 +0700

    Update

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -78,7 +78,7 @@ const UserLogin = () => {
                 className={classes.root}
                 justifyContent="center"
             >
-                <Grid item xs={4}>
+                <Grid item xs={8}>
                 <Fade in={checked} {...(checked ? { timeout: 1000 } : {})}>
                     <Card
                         className={classes.paper}
@@ -116,7 +116,7 @@ const UserLogin = () => {
                         </Card>
                     </Fade>
                 </Grid>
-                <Grid item xs={5}>
+                <Grid item xs={4}>
                 <Fade in={checked} {...(checked ? { timeout: 1000 } : {})}>
                     <Card
                         className={classes.paper}
