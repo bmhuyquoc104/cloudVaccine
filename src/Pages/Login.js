7c5762192fb5076a375c2cf6b86c2cf5d68e3f43
Kendrick2000 <s3814172@rmit.edu.vn>
2021-09-19 17:22:21 +0700

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -78,7 +78,7 @@ const UserLogin = () => {
                 className={classes.root}
                 justifyContent="center"
             >
-                <Grid item xs={8}>
+                <Grid item xs={6}>
                 <Fade in={checked} {...(checked ? { timeout: 1000 } : {})}>
                     <Card
                         className={classes.paper}
